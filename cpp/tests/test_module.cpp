5078e21c000cd6d37d0cb2cf6b1b2fabde403ebf
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2017-12-09 16:31:42 +0100

    Updating cpp template : removing useless tests & optimized compilation process

--- cpp/tests/test_module.cpp
+++ cpp/tests/test_module.cpp
@@ -1,11 +1,1 @@
 #include <lib/catch.hpp>
-
-TEST_CASE( "Test de test de module, lel", "[unit]")
-{
-    REQUIRE( 1 == 1 );
-}
-
-TEST_CASE( "Test qui ne marche pas, lel", "[fault]" )
-{
-    REQUIRE( 3.14 >= 4768 );
-}
